92499b74e2dcad28998523572fe48b10ab174a34
Roniclay Rodrigues <roniclayrodrigues07@gmail.com>
2023-12-25 18:28:46 -0300

    :recycle: feat: update comments

--- src/next-utils.ts
+++ src/next-utils.ts
@@ -2,9 +2,12 @@ import next from "next";
 
 const PORT = Number(process.env.PORT) || 3000;
 
+// Uma instância do aplicativo Next.js
 export const nextApp = next({
+  // indica que o aplicativo está em modo de desenvolvimento.
   dev: process.env.NODE_ENV !== "production",
   port: PORT,
 });
 
+//chamado para obter o manipulador de rotas do aplicativo Next.js.
 export const nextHandler = nextApp.getRequestHandler()
--- src/server.ts
+++ src/server.ts
@@ -16,15 +16,17 @@ const start = async () => {
     },
   });
 
+  // O nextHandler é responsável por direcionar a solicitação para a página apropriada do Next.js, garantindo que o roteamento do lado do servidor seja tratado corretamente.
   app.use((req, res) => nextHandler(req, res));
 
+  // garante que o servidor Express seja iniciado apenas quando o Next.js estiver pronto para lidar com as solicitações.
   nextApp.prepare().then(() => {
     //payload.logger.info('Next.js started')
 
     app.listen(PORT, async () => {
       //payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
-    })
-  })
+    });
+  });
 };
 
 start();
