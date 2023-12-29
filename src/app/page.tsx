7f791afa796d8f0610b7736d21330f104f78c4e2
Roniclay Rodrigues <roniclayrodrigues07@gmail.com>
2023-12-28 23:46:06 -0300

    :sparkles: feat: showing products in store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+import ProductReel from "@/components/ProductReel";
 
 const perks = [
   {
@@ -45,7 +46,7 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/* TODO: list products*/}
+        <ProductReel query={{sort: "desc", limit: 4}} title="Brand New" href="/products"/>
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
